2ce9c746a0ca240f88f09005bcc61a1001f55920
Maria Miroshnichenko <marg13@yandex.ru>
2024-01-17 10:41:58 +0000

    Update to Laravel 5.8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Observers\ProjectObserver;
-use App\Observers\TaskObserver;
 use App\Project;
 use App\Task;
 use Illuminate\Support\ServiceProvider;
@@ -17,8 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Project::observe(ProjectObserver::class);
-        Task::observe(TaskObserver::class);
+
     }
 
     /**
